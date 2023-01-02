654c68b55bd4a89f42e9b0507d131bc46b0ee0ef
kihopopo <104831865+kihopopo@users.noreply.github.com>
2023-01-02 13:28:15 +0000

    jQueryの練習

---
+++ jQuery-Lesson/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  $('body').html('<h1>Hello jQuery!!</h1>');
+});
+
+// 一行目の省略形
+// $(function(){
+//   jQueryプログラムの内容
+// });
+
+// 二行目
+// $( セレクタ ).メソッド ( パラメータ );
+// と記述
+
+// セレクタ……body
+// メソッド……html
+// パラメータ……'<h1>Hello JQuery!!</h1>'
