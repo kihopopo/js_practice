b111b96f08f1ccf3100d19282e15906349ea0c4b
kihopopo <104831865+kihopopo@users.noreply.github.com>
2023-01-03 09:51:46 +0000

    マウスオーバー時のCSS（class）をあらかじめ定義

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,12 +1,11 @@
 $(function(){
   $('.box1').mouseover(function(){
-    $('.box1').css({'background-color': '#0000FF'});
+    $('.box1').addClass('box1-ext');
   });
   $('.box1').mouseout(function(){
-    $('.box1').css({'background-color': '#FF0000'});
+    $('.box1').removeClass('box1-ext');
   });
 });
 
-// $('.セレクタ名').イベント名(function(){
-//   イベント発生時に行われる処理
-// });
+// addClass( )は、対象のHTML要素にclass属性を追加。
+// removeClass( )は、要素に設定されているclass属性が解除。
