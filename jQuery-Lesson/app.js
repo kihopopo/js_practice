352115cbb7e18a153726590a2261fd86f02f2f8f
kihopopo <104831865+kihopopo@users.noreply.github.com>
2023-01-03 09:55:13 +0000

    マウスクリックイベント

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,5 +1,17 @@
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').addClass('box1-ext');
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').removeClass('box1-ext');
+//   });
+// });
+
+// addClass( )は、対象のHTML要素にclass属性を追加。
+// removeClass( )は、要素に設定されているclass属性が解除。
+
 $(function(){
-  $('.box1').mouseover(function(){
+  $('.box1').on('click', function(){
     $('.box1').addClass('box1-ext');
   });
   $('.box1').mouseout(function(){
@@ -7,5 +19,7 @@ $(function(){
   });
 });
 
-// addClass( )は、対象のHTML要素にclass属性を追加。
-// removeClass( )は、要素に設定されているclass属性が解除。
+// マウスクリックイベント
+// $('.セレクタ名').on('click', function() {
+//   イベント発生時に行われる処理
+// });
